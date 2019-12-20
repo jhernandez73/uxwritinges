9ce0e37648b478772387db56344f3aea7be55524
Juan Cruz Hernandez <juan.hernandez@naranja.com>
2019-12-20 20:13:50 -0300

    Added metatags

--- pages/index.js
+++ pages/index.js
@@ -3,24 +3,23 @@ import Head from "next/head";
 export default () => (
   <div>
     <Head>
-      <!-- Primary Meta Tags -->
-<title>UX Writing en español.</title>
-<meta name="title" content="UX Writing en español.">
-<meta name="description" content="Por Caro Raspa y Sol Parnofiello. Primera comunidad hispana de escritores de producto digitales. ">
+      <title>UX Writing en español 💎</title>
+      <meta name="title" content="UX Writing en español." />
+      <meta name="description" content="Por Caro Raspa y Sol Parnofiello | Primera comunidad hispana de escritores de productos digitales." />
 
-<!-- Open Graph / Facebook -->
-<meta property="og:type" content="website">
-<meta property="og:url" content="https://metatags.io/">
-<meta property="og:title" content="UX Writing en español.">
-<meta property="og:description" content="Por Caro Raspa y Sol Parnofiello. Primera comunidad hispana de escritores de producto digitales. ">
-<meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
 
-<!-- Twitter -->
-<meta property="twitter:card" content="summary_large_image">
-<meta property="twitter:url" content="https://metatags.io/">
-<meta property="twitter:title" content="UX Writing en español.">
-<meta property="twitter:description" content="Por Caro Raspa y Sol Parnofiello. Primera comunidad hispana de escritores de producto digitales. ">
-<meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://metatags.io/" />
+      <meta property="og:title" content="UX Writing en español." />
+      <meta property="og:description" content="Por Caro Raspa y Sol Parnofiello. Primera comunidad hispana de escritores de producto digitales. "/>
+      <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
+
+
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://metatags.io/" />
+      <meta property="twitter:title" content="UX Writing en español." />
+      <meta property="twitter:description" content="Por Caro Raspa y Sol Parnofiello. Primera comunidad hispana de escritores de producto digitales. " />
+      <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
 
       <meta http-equiv="Content-Language" content="es" />
       <meta
